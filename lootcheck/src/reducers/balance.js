e745a32eb15e90fc26c8461c919b120cf0815287
JackyRecelis <jackymlui@live.com>
2019-04-30 15:18:37 +1000

    withdrawal tests and code written

--- lootcheck/src/reducers/balance.js
+++ lootcheck/src/reducers/balance.js
@@ -1,9 +1,13 @@
 import * as constants from '../actions/constants';
 
 const balance = (state = 0, action) => {
-    switch(action.type){
+    switch (action.type) {
         case constants.SET_BALANCE:
             return action.balance;
+        case constants.DEPOSIT:
+            return state + action.deposit;
+        case constants.WITHDRAW:
+            return state - action.withdraw;
         default:
             return state;
     }
--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -7,4 +7,16 @@ describe('balanceReducer', () => {
         expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance}))
         .toEqual(balance);
     });
+
+    it('deposits into the balance', ()=>{
+        const deposit = 10;
+        const initialState = 5;
+        expect(balanceReducer(initialState, { type:constants.DEPOSIT, deposit})).toBe(initialState+deposit);
+    });
+    it('withdraws from the balance', ()=>{
+        const withdraw = 10;
+        const initialState = 5;
+        expect(balanceReducer(initialState, { type:constants.WITHDRAW, withdraw})).toBe(initialState - withdraw);
+    });
 });
+
